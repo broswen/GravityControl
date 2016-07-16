7e0c5cf9cde44a4ed9918932e4fe27515d3417ec
Brad <broswen@gmail.com>
2016-07-16 13:48:56 -0500

    added zerog

--- src/com/broswen/gravitycontrol/GravityControl.java
+++ src/com/broswen/gravitycontrol/GravityControl.java
@@ -1,21 +1,49 @@
 package com.broswen.gravitycontrol;
 
 import org.bukkit.craftbukkit.v1_10_R1.Overridden;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Brad S. on 7/15/2016.
  */
-public class GravityControl extends JavaPlugin{
 
+public class GravityControl extends JavaPlugin implements Listener {
+
+    private static List<UUID> zeroG;
 
     @Override
     public void onEnable(){
+        zeroG = new ArrayList<>();
         //load config
     }
 
     @Override
     public void onDisable(){
+
+    }
+
+    public static boolean isZeroG(UUID uuid){
+        return zeroG.contains(uuid);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        Action a = e.getAction();
+        if(e.getItem() == null) return;
+        ItemStack item = e.getItem();
+
+
 
     }
 }
